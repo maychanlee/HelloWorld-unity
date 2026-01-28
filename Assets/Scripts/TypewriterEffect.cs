9cb32e3977100132271c6b008b405aadaf86a822
May Lee <mayhclee@gmail.com>
2026-01-27 21:46:05 -0800

    started dialogue box

---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypewriterEffect : MonoBehaviour
+{
+    [SerializeField] private float typewriterSpeed = 30f;
+    public Coroutine Run(string textToType, TMP_Text textLabel)
+    {
+        return StartCoroutine(routine:TypeText(textToType, textLabel));
+    }
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+        textLabel.text = string.Empty;
+
+        float t = 0;
+        int charIndex = 0;
+
+        while(charIndex < textToType.Length)
+        {
+            t += Time.deltaTime;
+            charIndex = Mathf.FloorToInt(t * typewriterSpeed);
+            charIndex = Mathf.Clamp(value:charIndex, min:0, max:textToType.Length);
+
+            textLabel.text = textToType.Substring(startIndex:0, length:charIndex);
+
+            yield return null;
+        }
+
+        textLabel.text = textToType;
+    }
+}
