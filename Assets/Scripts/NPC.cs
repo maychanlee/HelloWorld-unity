c6555a8b10f184ee2c4b556d8eeab4d1e8aa4786
May Lee <mayhclee@gmail.com>
2026-01-28 09:03:38 -0800

    Additional code to make dialogue works with buttons

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -7,11 +8,14 @@ using UnityEngine;
 public class NPC : MonoBehaviour, IInteractable
 {
     public NPCDialogue dialogueData;
-    public GameObject dialoguePanel;
-    public TMP_Text dialogueText, nameText;
-    
+    private DialogueController dialogueUI;
     private int dialogueIndex;
     private bool isTyping, isDialogueActive;
+
+    private void Start()
+    {
+        dialogueUI = DialogueController.Instance;
+    }
 
     public bool CanInteract()
     {
@@ -37,21 +41,38 @@ public class NPC : MonoBehaviour, IInteractable
         isDialogueActive = true;
         dialogueIndex = 0;
 
-        nameText.SetText(dialogueData.npcName);
-        dialoguePanel.SetActive(true);
-        StartCoroutine(TypeLine());
+        dialogueUI.SetNPCInfo(dialogueData.npcName);
+        dialogueUI.ShowDialogueUI(true);
+        DisplayCurrentLine();
     }
     void NextDialogueLine()
     {
         if (isTyping)
         {
             StopAllCoroutines();
-            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            dialogueUI.SetDialogueText(dialogueData.dialogueLines[dialogueIndex]);
             isTyping = false;
         }
-        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+
+        dialogueUI.ClearChoices();
+        if(dialogueData.endDialogueLines.Length > dialogueIndex && dialogueData.endDialogueLines[dialogueIndex])
         {
-            StartCoroutine(TypeLine());
+            EndDialogue();
+            return;
+        }
+
+        foreach (DialogueChoice dialogueChoice in dialogueData.dialogueChoices)
+        {
+            if(dialogueChoice.dialogueIndex == dialogueIndex)
+            {
+                DisplayChoices(dialogueChoice);
+                return;
+            }
+        }
+
+        if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            DisplayCurrentLine();
         }
         else
         {
@@ -61,11 +82,11 @@ public class NPC : MonoBehaviour, IInteractable
     IEnumerator TypeLine()
     {
         isTyping = true;
-        dialogueText.SetText("");
+        dialogueUI.SetDialogueText("");
 
         foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
         {
-            dialogueText.text += letter;
+            dialogueUI.SetDialogueText(dialogueUI.dialogueText.text += letter);
             yield return new WaitForSeconds(dialogueData.typeSpeed);
         }
         isTyping = false;
@@ -76,12 +97,35 @@ public class NPC : MonoBehaviour, IInteractable
             NextDialogueLine();
         }
     }
+
+    void DisplayChoices(DialogueChoice choice)
+    {
+        for (int i = 0; i < choice.dialogueChoices.Length; i++)
+        {
+            int nextIndex = choice.nextDialogueIndices[i];
+            dialogueUI.CreateChoiceButton(choice.dialogueChoices[i], () => ChooseOption(nextIndex));
+        }
+    }
+
+    void ChooseOption(int nextIndex)
+    {
+        dialogueIndex = nextIndex;
+        dialogueUI.ClearChoices();
+        DisplayCurrentLine();
+    }
+
+    void DisplayCurrentLine()
+    {
+        StopAllCoroutines();
+        StartCoroutine(TypeLine());
+    }
+
     public void EndDialogue()
     {
         StopAllCoroutines();
         isDialogueActive = false;
-        dialogueText.SetText("");
-        dialoguePanel.SetActive(false);
+        dialogueUI.SetDialogueText("");
+        dialogueUI.ShowDialogueUI(false);
     }
 
 }
--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -7,7 +7,63 @@ public class NPCDialogue : ScriptableObject
 {
     public string npcName;
     public string[] dialogueLines;
-    public bool[] autoProgressLines;
-    public float autoProgressDelay = 2.0f;
-    public float typeSpeed = 0.05f;
+    public bool[] autoProgressLines = new bool[] { true };
+    public bool[] endDialogueLines = new bool[] { false };
+    public float autoProgressDelay = 1.0f;
+    public float typeSpeed = 0.03f;
+    public DialogueChoice[] dialogueChoices;
+    
+    private void OnValidate()
+    {
+        if (dialogueLines != null)
+        {
+            if (autoProgressLines.Length != dialogueLines.Length)
+            {
+                bool[] newAutoProgressLines = new bool[dialogueLines.Length];
+                
+                // Copy existing values
+                for (int i = 0; i < newAutoProgressLines.Length; i++)
+                {
+                    if (i < autoProgressLines.Length)
+                    {
+                        newAutoProgressLines[i] = autoProgressLines[i];
+                    }
+                    else
+                    {
+                        // Default value for new entries
+                        newAutoProgressLines[i] = true;
+                    }
+                }
+                
+                autoProgressLines = newAutoProgressLines;
+            }
+            
+            if (endDialogueLines.Length != dialogueLines.Length)
+            {
+                bool[] newEndDialogueLines = new bool[dialogueLines.Length];
+                
+                for (int i = 0; i < newEndDialogueLines.Length; i++)
+                {
+                    if (i < endDialogueLines.Length)
+                    {
+                        newEndDialogueLines[i] = endDialogueLines[i];
+                    }
+                    else
+                    {
+                        newEndDialogueLines[i] = false;
+                    }
+                }
+                
+                endDialogueLines = newEndDialogueLines;
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class DialogueChoice
+{
+    public int dialogueIndex;
+    public string[] dialogueChoices;
+    public int[] nextDialogueIndices;
 }
